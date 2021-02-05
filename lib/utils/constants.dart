6c5e6de254c185c1203aa3f7a1b652f25e94faf5
vincent-musangu <vmusangu@gmail.com>
2021-02-05 21:33:16 +0200

    changed login info changed

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,4 @@
 const API_BASE_URL = "https://bp-agency-banking-api.herokuapp.com";
-const AgentPassword = "broadpay-8629350";
-const AgentNumber = "0979789839";
+const AgentPassword = "47251";
+const AgentNumber = "0963912233";
 const WalletNumber = "0973202178";
